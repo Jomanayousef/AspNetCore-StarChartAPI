1f6815ce9fcf2d49d956e7301979871e78b688b8
Jomanayousef <jojo.girl2012@gmail.com>
2020-07-06 20:45:04 +0300

    Completed module

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -18,6 +18,21 @@ namespace StarChart.Controllers
         {
             _context = context;
         }
-        
+        [HttpGet("{id:int}")]
+
+        public IActionResult GetById(int id)
+        {
+            return NotFound();
+        }
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            return NotFound();
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+
+        }
     }
 }
